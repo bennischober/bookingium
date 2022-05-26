08abc116856bd62437ca357d7bd1e4d99d055957
Benjamin Schober <benjamin.schober@stud.hs-kempten.de>
2022-05-26 16:04:21 +0200

    Added component for contact input

---
+++ components/ContactInput/index.tsx
@@ -0,0 +1,17 @@
+import { NumberInput, TextInput } from "@mantine/core";
+import { InputComponentProps } from "../../types";
+
+// Note: This is no standalone component, this needs to be paired with a functional form component!
+export default function ContactInput({ Form }: InputComponentProps<any>) {
+    return (
+        <>
+            <TextInput label="Email" {...Form.getInputProps("email")} />
+            <TextInput label="Phone" {...Form.getInputProps("phone")} />
+            <TextInput
+                label="Mobile phone"
+                {...Form.getInputProps("mobilePhone")}
+            />
+            <TextInput label="Homepage" {...Form.getInputProps("homepage")} />
+        </>
+    );
+}
