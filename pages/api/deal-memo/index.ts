1872196c3cbae438c05aa250177a7bb2fad35060
Benjamin Schober <benjamin.schober@stud.hs-kempten.de>
2022-05-26 18:23:29 +0200

    Updated deal memo api
    * removed unused imports and comments
    * added fallback if data populating does not work

--- pages/api/deal-memo/index.ts
+++ pages/api/deal-memo/index.ts
@@ -1,51 +1,11 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { connect } from '../../../lib/mongodb';
 import DealMemo from '../../../models/deal-memo';
-import {Band} from "../../../models/band";
-import mongoose from "mongoose";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
     const { method } = req;
 
     await connect();
-
-    /*
-    TESTING:
-
-        const b = new Band({
-        _id: new mongoose.Types.ObjectId(),
-        bandid: 'dc6d3f1d-7605-4c3f-902c-ea46517ae642',
-        name: 'test',
-        notes: "test notes",
-        dm: {
-            userid: 'bb231c4e-277e-4a92-b8cf-79b385259a03',
-            created: '2020-01-01T00:00:00Z[UTC]',
-            edited: '2020-01-01T00:00:00Z[UTC]'
-        }
-    });
-
-    b.save();
-
-    const dm = await DealMemo.create({
-        dealId: '471ac508-c3f7-4c99-be42-75d3dc1dfa5c', deal: 'deal text, lol', date: "some random not date date", price: 12000, posters: 2500, notes: "some shitty notes",
-        bandid: b._id,
-        dm: {
-            userid: 'bb231c4e-277e-4a92-b8cf-79b385259a03',
-            created: '2020-01-01T00:00:00Z[Z]',
-            edited: '2020-01-01T00:00:00Z[Z]',
-        }
-    });
-
-    DealMemo.findOne().populate('bandid').exec((err, dealMemo) => {
-        if (err) {
-            console.log(err);
-            return;
-        }
-        console.log(dealMemo);
-    });
-    */
-
-    console.log(req.body);
 
     switch (method) {
         case 'GET':
@@ -57,12 +17,13 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
                     }
                     res.status(200).json({ success: true, data: dealMemos });
                 });
-
-                // or standalone data?
-                // const dealMemos = await DealMemo.find({}); // get all entries
-                // res.status(200).json({ success: true, data: dealMemos });
             } catch (error) {
-                res.status(500).json({ success: false, error: error });
+                // or standalone data?
+                const dealMemos = await DealMemo.find({}); // get all entries
+                res.status(200).json({ success: true, data: dealMemos });
+
+                // send all data instead of population error?
+                //res.status(500).json({ success: false, error: error });
             }
             break;
 
