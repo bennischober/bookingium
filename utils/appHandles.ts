44b8071d78fcf4afac939bbfcf2a3b468d166f94
Benjamin Schober <benjamin.schober@stud.hs-kempten.de>
2022-05-29 16:23:55 +0200

    Added getBands and getMemos data fetch to appHandles

--- utils/appHandles.ts
+++ utils/appHandles.ts
@@ -2,6 +2,7 @@ import { MantineTheme } from "@mantine/core";
 import { NextRouter } from "next/router";
 import dayjs from 'dayjs';
 import { SessionProps } from "../types";
+import axios from "axios";
 
 // handle theme, language, and other app settings
 
@@ -42,7 +43,7 @@ export function handleSession(router: NextRouter, session: SessionProps["session
     }
 }
 
-/** --- OTHER HADNLE --- **/
+/** --- OTHER HANDLE --- **/
 
 export function getCurrentYear() {
     return dayjs().format("YYYY");
@@ -58,4 +59,33 @@ export function getNameInitials(name: string) {
 export function addToArray(array: any[], index: number, item: any) {
     array.splice(index, 0, item);
     return array;
+}
+
+/** --- FETCH HANDLE --- **/
+export const getBands = async (session: SessionProps["session"]) => {
+    // get url form links.ts
+    const res = await axios.get("http://localhost:3000/api/band", {
+        params: {
+            userid: session.userid,
+        },
+    });
+    const bands = await res.data.data;
+    if (res.status !== 200) return;
+    return bands
+};
+
+export const getMemos = async (session: SessionProps["session"]) => {
+    const res = await axios.get("http://localhost:3000/api/deal-memo", {
+        params: {
+            userid: session.userid,
+        },
+    });
+    if (res.status !== 200) return;
+    const memos = await res.data.data;
+    return memos;
+};
+
+/** --- DATABASE HANDLE --- **/
+export function isPopulated<T>(obj: T | any) : obj is T {
+    return (obj && obj.name && typeof obj.name === 'string');
 }
