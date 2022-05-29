cc2de9b2a90796e5d0a764270b8ef68265b888f5
Benjamin Schober <benjamin.schober@stud.hs-kempten.de>
2022-05-29 16:26:39 +0200

    Updated specific deal memo page

--- pages/deal-memo/[id]/index.tsx
+++ pages/deal-memo/[id]/index.tsx
@@ -1,22 +1,74 @@
-import { Button, Text } from "@mantine/core";
+import { Button, Paper, Space, Tabs, Text, Title } from "@mantine/core";
+import { showNotification, updateNotification } from "@mantine/notifications";
 import axios from "axios";
+import dayjs from "dayjs";
 import { GetServerSideProps } from "next";
+import { getSession } from "next-auth/react";
 import { useRouter } from "next/router";
-import { MdArrowBack } from "react-icons/md";
+import { useEffect, useState } from "react";
+import { MdArrowBack, MdCheck, MdClose } from "react-icons/md";
+import { DealEditForm } from "../../../components/DealMemoForm";
+import { IBand } from "../../../models/band";
 import { IDealMemo } from "../../../models/deal-memo";
-import { goToLastRoute } from "../../../utils/appHandles";
+import { CompleteDealMemoPageProps } from "../../../types";
+import { goToLastRoute, isPopulated } from "../../../utils/appHandles";
 
 // move interface to types file
 // move jsx stuff to new component
 
-export interface CompleteDealMemoPageProps {
-    memo: IDealMemo;
-}
-
 export default function CompleteDealMemoPage({
     memo,
+    session,
 }: CompleteDealMemoPageProps) {
+    const [bandData, setBandData] = useState<IBand>({} as IBand);
+    const [memoData, setMemoData] = useState<IDealMemo>(memo);
     const router = useRouter();
+
+    useEffect(() => {
+        if (isPopulated<IBand>(memo.bandid)) {
+            setBandData(memo.bandid);
+        }
+    }, [bandData]);
+
+    const handleMemos = async (data: {}) => {
+        showNotification({
+            id: "load-data",
+            loading: true,
+            title: "Saving your data",
+            message:
+                "You will be notified wethere your data is saved or any problem occured",
+            autoClose: false,
+            disallowClose: true,
+        });
+
+        const res = await axios.put(
+            `http://localhost:3000/api/deal-memo/${memo._id}`,
+            { data: data }
+        );
+
+        if (res.status === 200) {
+            setMemoData(res.data.data);
+
+            updateNotification({
+                id: "load-data",
+                color: "teal",
+                title: "Data saved",
+                message: "Your data has been successsfully updated",
+                icon: <MdCheck />,
+                autoClose: 2000,
+            });
+            return;
+        }
+        // an error occured, show notification
+        updateNotification({
+            id: "load-data",
+            color: "red",
+            title: "An error occured",
+            message: "Your data could not be saved",
+            icon: <MdClose />,
+            autoClose: 2000,
+        });
+    };
 
     return (
         <>
@@ -27,15 +79,36 @@ export default function CompleteDealMemoPage({
             >
                 <Text>Go back</Text>
             </Button>
-            <h1>Complete Deal Memos</h1>
-            {memo.dealId}
+            <Space h="xl" />
+            <Title>Deal: {memoData.dealId}</Title>
+            <Space h="xl" />
+            <Tabs>
+                <Tabs.Tab label="Deal data">
+                    <Paper withBorder shadow="md" p={30} mt={30} radius="xs">
+                        <DealEditForm
+                            handleMemos={handleMemos}
+                            session={session}
+                            data={{
+                                deal: memoData.deal,
+                                date: dayjs(memoData.date).toDate(),
+                                price: memoData.price,
+                                posters: memoData.posters,
+                                notes: memoData.notes,
+                            }}
+                            bandName={bandData.name}
+                        />
+                    </Paper>
+                </Tabs.Tab>
+                <Tabs.Tab label="Band data"></Tabs.Tab>
+            </Tabs>
         </>
     );
 }
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
+    const session = await getSession({ req: ctx.req });
     const id = ctx.query.id;
     const res = await axios.get(`http://localhost:3000/api/deal-memo/${id}`);
     const data = await res.data;
-    return { props: { memo: data.data } };
+    return { props: { session: session, memo: data.data } };
 };
