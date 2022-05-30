32f32290796274c44386d6629c58ff7d1bc148c0
Benjamin Schober <benjamin.schober@stud.hs-kempten.de>
2022-05-30 23:02:12 +0200

    Removed unused api files
    * this is due to design changes of deal memo creation

--- pages/api/deal-memo/add/hotel.ts
+++
@@ -1,8 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { connect } from '../../../../lib/mongodb';
-import DealMemo from '../../../../models/deal-memo';
-import Band from "../../../../models/hotel";
-
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-    const { method, body } = req;
-}
