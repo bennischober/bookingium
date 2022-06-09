dcaa64f291bc4af60af0a5f8980550a244d1bce8
Benjamin Schober <benjamin.schober@stud.hs-kempten.de>
2022-06-09 19:56:27 +0200

    Updated navigation links

--- utils/links.ts
+++ utils/links.ts
@@ -1,21 +1,35 @@
 import { MdCreateNewFolder, MdDashboard } from "react-icons/md";
 import { RiFileList3Line, RiFileTextLine } from "react-icons/ri";
-import {GoFile} from "react-icons/go";
+import { MdListAlt, MdPostAdd, MdHome } from "react-icons/md";
 
+const links = {
+    root: "http://localhost:3000",
+    auth: {
+        login: "/auth/login",
+        register: "/auth/register",
+    },
+    user: {
+        dashboard: "/user/dashboard",
+        profile: "/user/profile",
+        settings: "/user/settings",
+    },
+    add: {
+        band: "/add/band",
+        dealMemo: "/add/deal-memo",
+        hotel: "/add/hotel",
+        lopro: "/add/lopro",
+        venue: "/add/venue",
+    },
+    create: {
+        dealMemo: "/create/deal-memo",
+        itinerary: "/create/itinerary",
+        contract: "/create/contract",
+        invoice: "/create/invoice",
+    }
+}
 
 export function getLinks() {
-    return {
-        "root": "http://localhost:3000",
-        "user": {
-            "login": "/auth/login",
-            "dasboard": "/user/dashboard",
-            "profile": "/user/profile",
-            "settings": "/user/settings",
-        },
-        "api": {
-
-        }
-    };
+    return links;
 }
 
 export function getFooterData() {
@@ -29,19 +43,27 @@ export function getFooterData() {
     ];
 }
 
+// Refactor all paths!
 export function getNavbarData() {
     return [
-        { label: "Dashboard", icon: MdDashboard, link: "/" },
-        { label: "Deal Memo", icon: GoFile, link: "/deal-memo" },
-        { label: "Bands", icon: RiFileList3Line, links: [] },
-        { label: "Invoice", icon: RiFileList3Line, link: "/invoice" },
+        { label: "Home", icon: MdHome, link: "/" },
+        { label: "Deal Memo", icon: RiFileTextLine, link: "/add/deal-memo" },
         { label: "Contract", icon: RiFileTextLine, link: "/contract" },
         { label: "Itinerary", icon: RiFileTextLine, link: "/itinerary" },
-        { label: "Create new", icon: MdCreateNewFolder, links: [
-            { label: "Band", link: "/new/band" },
-            { label: "Location", link: "/new/location" },
-            { label: "Promoter", link: "/new/promoter" },
-            { label: "Hotel", link: "/new/hotel" },
-        ] },
+        { label: "Invoice", icon: RiFileList3Line, link: "/invoice" },
+        {
+            label: "Lists", icon: MdListAlt, links: [
+                { label: "Deal Memo List", icon: MdListAlt, link: "/deal-memo" },
+            ]
+        },
+        {
+            label: "Create", icon: MdPostAdd, links: [
+                { label: "Deal Memo", link: "/add/deal-memo" },
+                { label: "Band", link: "/new/band" },
+                { label: "Location", link: "/new/location" },
+                { label: "Promoter", link: "/new/promoter" },
+                { label: "Hotel", link: "/new/hotel" },
+            ]
+        },
     ];
 }
