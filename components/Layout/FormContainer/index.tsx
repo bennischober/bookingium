56928fa6616d2d4310bc7236706ad452b16bd88e
Benjamin Schober <benjamin.schober@stud.hs-kempten.de>
2022-07-30 18:00:42 +0200

    Added FormContainer component to use same styles

---
+++ components/Layout/FormContainer/index.tsx
@@ -0,0 +1,14 @@
+import { Center, Paper } from "@mantine/core";
+import { FormContainerProps } from "../../../types";
+
+export function FormContainer({ children, center, sx }: FormContainerProps) {
+    const style = sx ?? { minWidth: 300, maxWidth: 750, width: "100vw" };
+    const cent = center ?? true;
+    const paper = (
+        <Paper withBorder shadow="md" p={30} mt={30} radius="xs" sx={style}>
+            {children}
+        </Paper>
+    );
+    const comp = cent ? <Center>{paper}</Center> : paper;
+    return comp;
+}
