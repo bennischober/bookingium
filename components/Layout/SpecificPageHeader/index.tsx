a000208c430da98dd620fbf2aec048f4e9ec35a6
Benjamin Schober <benjamin.schober@stud.hs-kempten.de>
2022-08-28 01:04:42 +0200

    Updated layout and style of SpecificPageHeader
    * added Space and Paper

--- components/Layout/SpecificPageHeader/index.tsx
+++ components/Layout/SpecificPageHeader/index.tsx
@@ -1,4 +1,4 @@
-import { Space, Title } from "@mantine/core";
+import { Paper, Space, Title } from "@mantine/core";
 import { BackButton } from "../../LayoutElements/BackButton";
 import { SpecificPageHeaderProps } from "../../../types";
 
@@ -11,15 +11,24 @@ export function SpecificPageHeader({
 }: SpecificPageHeaderProps) {
     const useBack = useBackButton ?? true;
 
-    return(
+    return (
         <>
-                        {useBack ? <BackButton /> : null}
-                <Space h="xl" />
-                <Title>{titleName}: {title}</Title>
-                <Space h="xl" />
-                {subTitle ? <Title order={2}>{subTitle}</Title> : null}
-                {other}
-                <Space h="xl" />
+            {useBack ? <BackButton /> : null}
+            <Space h="xl" />
+            <Paper shadow="xs" p="xl" radius="xs">
+                <Title>
+                    {titleName}: {title}
+                </Title>
+                <Space h="md" />
+                {subTitle ? <Title order={3}>{subTitle}</Title> : null}
+                {other ? (
+                    <>
+                        <Space h="md" />
+                        {other}
+                    </>
+                ) : null}
+            </Paper>
+            <Space h="xl" />
         </>
     );
 }
