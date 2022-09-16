58bcf0f541575cabbd84c0711d2439d95bf25d78
Benjamin Schober <benjamin.schober@stud.hs-kempten.de>
2022-09-16 12:12:02 +0200

    Removed unused model types and objects

--- models/modelObjects.ts
+++ models/modelObjects.ts
@@ -22,33 +22,8 @@ export const OContact = {
     homepage: { type: String, default: '' },
 };
 
-export const OCompany = {
-    name: { type: String, default: '' },
-    vatNumber: { type: String, default: '' },
-    ustNumber: { type: String, default: '' },
-    address: OAddress,
-    contact: OContact,
-}
-
 export const ODm = {
     userid: { type: String, required: true },
     created: { type: String, default: dayjs().format('YYYY-MM-DDTHH:mm:ssZ[Z]') },
     edited: { type: String, default: dayjs().format('YYYY-MM-DDTHH:mm:ssZ[Z]') }
-}
-
-export const OPerson = {
-    name: { type: String, default: '' },
-    lastname: { type: String, default: '' },
-    email: { type: String, default: '' },
-    phone: { type: String, default: '' },
-    mobilePhone: { type: String, default: '' },
-    role: { type: String, default: '' },
-    notes: { type: String, default: '' },
-}
-
-export const OContactPerson = {
-    name: { type: String, default: '' },
-    email: { type: String, default: '' },
-    phone: { type: String, default: '' },
-    role: { type: String, default: '' },
 }
--- models/modelTypes.d.ts
+++ models/modelTypes.d.ts
@@ -9,18 +9,14 @@ export interface IAddress {
     countryCode: string;
 }
 
-export interface ICompany {
-    name: string;
-    vatNumber: string;
-    ustNumber: string;
-    address: IAddress;
-    contact: IContact;
-}
-
 export interface IContact {
+    email: string;
     phone: string;
     mobilePhone: string;
-    email: string;
+    otherNumbers: {
+        identifier: string;
+        number: string;
+    }[];
     homepage: string;
 }
 
@@ -28,22 +24,4 @@ export interface IDm {
     userid: string;
     created: string;
     edited: string;
-}
-
-export interface IContactPerson {
-    name: string;
-    role: string;
-    email: string;
-    phone: string;
-}
-// add Person schema!
-// add birthday, organization, and address? also encapuslate contact?
-export interface IPerson {
-    name: string;
-    lastname: string;
-    email: string;
-    phone: string;
-    mobilePhone: string;
-    role?: string;
-    notes?: string;
 }
