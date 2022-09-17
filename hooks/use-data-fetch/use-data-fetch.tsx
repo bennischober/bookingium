e0aacaf2c6794429cd7968c3919eb958b69072bb
Benjamin Schober <benjamin.schober@stud.hs-kempten.de>
2022-09-17 11:35:57 +0200

    Updated use-data-fetch hook
    * now saves 3 states: data, loading, error

--- hooks/use-data-fetch/use-data-fetch.tsx
+++ hooks/use-data-fetch/use-data-fetch.tsx
@@ -1,22 +1,37 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 /**
  * This hook is used to fetch data from any source. It saves the data in the state and updates it, if the data changes.
  * @param url the url to fetch data
  * @param {any=}options config for axios request
- * @returns {Promise<T|undefined>} the data or undefined if the data is not fetched yet
+ * @returns {{data: T, error: any, loading: boolean}} the data or an empty array, if the data is not fetched yet
  */
-export async function useDataFetch<T>(
+export function useDataFetch<T>(
     url: string,
     options?: any
-): Promise<T | undefined> {
-    const [data, setData] = useState<T>();
+): { data: T; error: any; loading: boolean } {
+    // default data might work => test
+    const [data, setData] = useState<T>([] as unknown as T);
+    const [error, setError] = useState<any>();
+    const [loading, setLoading] = useState<boolean>(false);
 
-    // fetch with axios inline
-    await axios.get(url, options).then((response) => {
-        setData(response.data);
-    });
+    useEffect(() => {
+        const fetch = async () => {
+            try {
+                setLoading(true);
+                const response = await axios.get(url, options);
+                console.log(response);
+                setData(response.data.data);
+            } catch (error: any) {
+                setError(error);
+                console.log(error);
+            } finally {
+                setLoading(false);
+            }
+        };
+        fetch();
+    }, [url]);
 
-    return data;
+    return { data, error, loading };
 }
