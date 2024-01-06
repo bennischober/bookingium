4af28a70c05cd39fa7954f451e5fa7ee192df6d2
Benjamin Schober <benjamin.schober@stud.hs-kempten.de>
2024-01-06 22:35:03 +0100

    Optimize fetching in deal-memo head page
    * add parallel data fetching
    * remove regex in band name creation

--- app/(main)/deal-memo/[id]/@head/page.tsx
+++ app/(main)/deal-memo/[id]/@head/page.tsx
@@ -8,58 +8,85 @@ import { HeadComponent } from "./component";
 import { ICompany } from "@/models/company";
 import { IPerson } from "@/models/person";
 import { IWorkplace } from "@/models/workplace";
+import { Session } from "next-auth";
 
-export default async function Page({ params }: { params: { id: string } }) {
-    const session = await auth();
-    if (!session) return null;
+async function fetchData(id: string, session: Session) {
+    const params = { userid: session.userid };
+    // parallel fetch data
+    const memoData = serverSideFetch<IDealMemo>(`/api/deal-memo/${id}`, params);
+    const wpData = serverSideFetch<IWorkplace>("/api/user/workplace", params);
+    const [memo, workplace] = await Promise.all([memoData, wpData]);
 
-    const memo = await serverSideFetch<IDealMemo>(
-        `/api/deal-memo/${params.id}`,
-        {
-            userid: session.userid,
-        }
-    );
-
+    // ensure data population
     const band = isPopulated<IBand>(memo.bandid)
         ? (memo.bandid as IBand)
         : ({} as IBand);
-
-    const bandCompany = await serverSideFetch<ICompany>(
-        `api/company/${band?.company}`,
-        {
-            userid: session?.userid,
-        }
-    );
-
-    const bandResponsiblePerson = await serverSideFetch<IPerson>(
-        `api/band/${band?._id}/search/person`,
-        {
-            userid: session?.userid,
-            role: "TL",
-        }
-    );
-
     const loproCompany = isPopulated<ICompany>(memo.lopro.company)
         ? (memo.lopro.company as ICompany)
         : ({} as ICompany);
     const loproPerson = isPopulated<IPerson>(memo.lopro.person)
         ? (memo.lopro.person as IPerson)
         : ({} as IPerson);
-
     const venue = isPopulated<IVenue>(memo.venueid)
         ? (memo.venueid as IVenue)
         : ({} as IVenue);
 
-    const workplace = await serverSideFetch<IWorkplace>("/api/user/workplace", {
-        userid: session?.userid,
-    });
+    // parallel fetch data
+    const bandCompanyData = serverSideFetch<ICompany>(
+        `api/company/${band?.company}`,
+        params
+    );
+    const bandResponsiblePersonData = serverSideFetch<IPerson>(
+        `api/band/${band?._id}/search/person`,
+        {
+            userid: session?.userid,
+            role: "TL",
+        }
+    );
+    const [bandCompany, bandResponsiblePerson] = await Promise.all([
+        bandCompanyData,
+        bandResponsiblePersonData,
+    ]);
+
+    return {
+        memo,
+        band,
+        bandCompany,
+        bandResponsiblePerson,
+        loproCompany,
+        loproPerson,
+        venue,
+        workplace,
+    };
+}
+
+export default async function Page({ params }: { params: { id: string } }) {
+    const session = await auth();
+    if (!session) return null;
+
+    const {
+        memo,
+        band,
+        bandCompany,
+        bandResponsiblePerson,
+        loproCompany,
+        loproPerson,
+        venue,
+        workplace,
+    } = await fetchData(params.id, session);
 
     // ensure, that band?.name has no whitespaces, is lowercase and has no special characters
     var bandName = band?.name
-        .replace(/\s/g, "")
-        .toLowerCase()
-        .replace(/[^a-zA-Z ]/g, "")
-        .replace(/\s/g, "");
+        .split("")
+        .map((char) => char.toLowerCase())
+        .filter(
+            (char) =>
+                (char >= "a" && char <= "z") ||
+                (char >= "0" && char <= "9") ||
+                char === " "
+        )
+        .filter((char) => char !== " ")
+        .join("");
     const date = dayjs(memo.date).format("DDMMYY");
     const filename = bandName + "_contract_" + date;
 
